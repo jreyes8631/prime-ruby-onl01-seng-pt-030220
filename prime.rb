061f06a798f97739f6879854cdbc0b7d99618883
Jose E Reyes <jreyes8631@gmail.com>
2020-03-13 22:50:18 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,13 +1,8 @@
 # Add  code here!
 
 def sieve(max)
-  # Set up an array with all the numbers from 0 to the max
   primes = (0..max).to_a
-
-
   primes[0] = primes[1] = nil
-
-
   counter = 0
   primes.each do |p|
     # Skip if nil
@@ -17,7 +12,6 @@ def sieve(max)
     (p*p).step(max,p) { |m| primes[m] = nil }
   end
 
-  # Finally, return the compacted array.
   puts "Solved for #{max} in #{counter} steps."
   primes.compact
 end
